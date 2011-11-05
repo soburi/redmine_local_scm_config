fcccd5e6b8a14dda315ae9c4a161777e117f8b17
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2011-11-06 00:02:25 +0900

    Update Author's URL.

--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'local_scm_config_hook'
 
 Redmine::Plugin.register :redmine_local_scm_config do
   name 'Redmine Local Scm Config Editor plugin'
-  author 'Author name'
+  author 'TOKITA Hiroshi'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/soburi/redmine_local_scm_config'
+  author_url 'https://github.com/soburi/'
 end
